ad2e8ab4f3ae7b21cc44f48d91f8452266e2e0c0
chetkurisai <103044606+chetkurisai@users.noreply.github.com>
2025-02-06 00:06:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import joblib
 import argparse
 import os
 import numpy as np
+import gcsfs  # Import GCS File System
 
 def train_and_save_model(X_train, y_train, model_output_path):
     model = RandomForestRegressor(random_state=42)
@@ -20,14 +21,27 @@ def train_and_save_model(X_train, y_train, model_output_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--model-output-path', dest='model_output_path',
-                        default=r'D:\ml_prjects\Projects\BostonHousing_prediction\Model', type=str, 
-                        help='Path to save the trained model')
+                        default='gs://ml_bucket_p1/model', type=str, 
+                        help='Path to save the trained model in GCS')
     args = parser.parse_args()
 
-    # Load training data
-    data_path = r'D:\ml_prjects\Projects\BostonHousing_prediction'
-    X_train = np.load(os.path.join(data_path, "X_train.npy"))
-    y_train = np.load(os.path.join(data_path, "y_train.npy"))
+    # GCS Bucket Information
+    BUCKET_NAME = "ml_bucket_p1"
+    DATA_PATH = f"gs://{BUCKET_NAME}/processed_data"  # Updated to your GCS folder
+
+    # Use GCS File System to Load Data
+    fs = gcsfs.GCSFileSystem()
+
+    with fs.open(os.path.join(DATA_PATH, "X_train.npy"), "rb") as f:
+        X_train = np.load(f)
+
+    with fs.open(os.path.join(DATA_PATH, "y_train.npy"), "rb") as f:
+        y_train = np.load(f)
 
     # Train and save the model
     train_and_save_model(X_train, y_train, args.model_output_path)
+
+    # Upload the model to GCS
+    model_gcs_path = os.path.join(args.model_output_path, "boston_housing_model.joblib")
+    fs.put("boston_housing_model.joblib", model_gcs_path)
+    print(f"Model uploaded to {model_gcs_path}")
